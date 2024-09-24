be87cdb48d02f0a620a053e4ff7ba130ec21198b
Waggerok <115732069+Waggerok@users.noreply.github.com>
2024-09-24 14:18:04 +0300

    initilize bot

---
+++ bot/index.js
@@ -0,0 +1,22 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+//Инфа по боту, токены и т.д
+const token = '7744699540:AAH375uB1jJkg-7nBOp4YLPHGrxWv96LNHQ';
+const bot = new TelegramBot(token, {polling : true});
+
+bot.on('message', async(msg) => {
+    const chatId = msg.chat.id;
+    const text = msg.text;
+
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'Привет! Мы открыли наш интернет-магазин прямо в телеграм, нажимай на кнопку снизу, чтобы зайти в мини-приложение', {
+            reply_markup: {
+                inline_keyboard : [
+                    [
+                        {text : 'Открыть магазин в Telegram-mini-app'}
+                    ]
+                ]
+            }
+        });
+    }
+})
